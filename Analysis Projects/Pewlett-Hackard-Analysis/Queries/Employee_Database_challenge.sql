913a4b65a4becc0bd3bb615c9943a2b93e21f2b4
tarini-mi7 <tarini.pal123@gmail.com>
2022-05-29 10:41:29 -0700

    Images addes.

--- Analysis Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -32,3 +32,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count Desc;
+
+ --mentorship_eligibilty
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	   e.first_name, 
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN title as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
